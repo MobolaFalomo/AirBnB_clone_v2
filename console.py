40d72baec32a56a58b41c8a1bd02a5a44984cb67
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-19 16:05:47 -0700

    mysqldev

--- console.py
+++ console.py
@@ -115,6 +115,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        tokens = args.split()
+
+        # extract class name and params
+	class_name = tokens[0]
+	params = tokens[1:]
+
         if not args:
             print("** class name missing **")
             return
@@ -122,6 +128,28 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         new_instance = HBNBCommand.classes[args]()
+
+	for param in params:
+	    try:
+	        # splitting each params items to key and values
+	        k, v = param.split("=")
+                # replacing the underscore with whitespace
+		v = v.replace("_", " ")
+		v = v.replace('"', '\\')
+		if v[0] == '"' and v[-1] == '"' and len(v) > 1:
+		    v = v[1:-1]
+                # if the value contains .
+                if "." in v:
+		    # convert to a float
+		    v = float(v)
+	        else:
+		    # convert to an integer
+		    v = int(v)
+                setattr(new_instance, k, v)
+
+	    except ValueError:
+	        continue
+
         storage.save()
         print(new_instance.id)
         storage.save()
---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+--A script that prepares a MySQL server 
+--Creates a database hbnb_test_db
+--Creates a new user hbnb_test (in localhost)
+--The password is set to hbnb_test_pwd
+--hbnb_test should have all privileges on the database hbnb_test_db
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnh_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
