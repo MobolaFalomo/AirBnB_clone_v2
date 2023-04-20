a3f0119ee8e1657a783a8c5ee53cf0eb8e973b8e
Mobola Falomo <mobolafalomo@gmail.com>
2023-04-19 17:16:43 -0700

    updates

--- console.py
+++ console.py
@@ -127,8 +127,8 @@ class HBNBCommand(cmd.Cmd):
         tokens = args.split()
 
         # extract class name and params
-	class_name = tokens[0]
-	params = tokens[1:]
+        class_name = tokens[0]
+        params = tokens[1:]
 
         if not args:
             print("** class name missing **")
@@ -138,22 +138,22 @@ class HBNBCommand(cmd.Cmd):
             return
         new_instance = HBNBCommand.classes[args]()
 
-	for param in params:
-	    try:
+        for param in params:
+            try:
 	        # splitting each params items to key and values
 	        k, v = param.split("=")
                 # replacing the underscore with whitespace
-		v = v.replace("_", " ")
-		v = v.replace('"', '\\')
-		if v[0] == '"' and v[-1] == '"' and len(v) > 1:
-		    v = v[1:-1]
+                v = v.replace("_", " ")
+                v = v.replace('"', '\\')
+                if v[0] == '"' and v[-1] == '"' and len(v) > 1:
+                    v = v[1:-1]
                 # if the value contains .
                 elif "." in v:
-		    # convert to a float
-		    v = float(v)
+                    # convert to a float
+                    v = float(v)
 	        else:
-		    # convert to an integer
-		    v = int(v)
+                    # convert to an integer
+                    v = int(v)
                 setattr(new_instance, k, v)
 
 	    except ValueError:
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@
 --hbnb_test should have all privileges on the database hbnb_test_db
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnh_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS hbnh_dev@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@'localhost';
+GRANT SELECT ON performance_schema.* TO hbnb_dev@'localhost';
 FLUSH PRIVILEGES
